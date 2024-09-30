13646bb9ca8c7c35e38dea05d577791750928a07
Prathamesh <prathamesh10082004@gmail.com>
2024-09-30 16:56:33 +0530

    seventh_Commit

--- src/main/java/com/prathamesh/taskmanager/Filters/CorsConfig.java
+++ src/main/java/com/prathamesh/taskmanager/Filters/CorsConfig.java
@@ -5,14 +5,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
+@Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsConfig extends OncePerRequestFilter {
 
