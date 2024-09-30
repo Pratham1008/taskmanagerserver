5d9d45e169ee374d518300dbf25cfe4e71b6d9ff
Prathamesh <prathamesh10082004@gmail.com>
2024-09-30 12:58:45 +0530

    six_Commit

--- src/main/java/com/prathamesh/taskmanager/Filters/CorsFilter.java
+++ src/main/java/com/prathamesh/taskmanager/Filters/CorsFilter.java
@@ -18,7 +18,7 @@ public class CorsFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,@NonNull FilterChain chain) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Origin", "https://task-manager-sable-phi.vercel.app");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, X-Requested-With");
         response.setHeader("Access-Control-Max-Age", "3600");
