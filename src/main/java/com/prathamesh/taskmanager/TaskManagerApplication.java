f3c35b918e1b29be296f0b0e22bb3c89b75f63f6
Prathamesh <prathamesh10082004@gmail.com>
2024-10-13 15:05:27 +0530

    Error updates

--- src/main/java/com/prathamesh/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/prathamesh/taskmanager/TaskManagerApplication.java
@@ -2,14 +2,12 @@ package com.prathamesh.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @Controller
-public class TaskManagerApplication implements ErrorController {
+public class TaskManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
@@ -18,10 +16,5 @@ public class TaskManagerApplication implements ErrorController {
     @GetMapping("/")
     public String index(){
         return "Desclaimer";
-    }
-
-    @RequestMapping("/error")
-    public String error(){
-        return "pagenotfound";
     }
 }
